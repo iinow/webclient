61fd279f6fdbcac8a71c00362f1e38c8817e1594
이준하/BT개발실/NO <joonha.lee@nhnservice.com>
2020-03-01 19:35:21 +0900

    init commit

---
+++ pages/webclient.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Hello
+    <button @click="hello2('dfdfd')">콜</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  mounted () {
+    console.log('아...')
+  },
+  methods: {
+    hello: function (str:string): string {
+      console.log(str)
+      return str
+    },
+    hello2 (str: string): string {
+      console.log(str)
+      return str
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
